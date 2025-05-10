0fdfdd7578f4b9f363f02a2c6de01c38301d227f
L4K6 <je.pavel@seznam.cz>
2025-05-10 14:01:10 +0200

    finished product

--- main.py
+++ main.py
@@ -2,9 +2,21 @@ def main():
     path_to_file = "books/frankenstein.txt"
     text = get_book_text(path_to_file)
     count = get_word_count(text)
+    #counting_letters is dictionary
     counting_letters = char_counter(text)
-    print(f"{count} words found in the document")
-    print(counting_letters)
+    sorted_dictionary = sorted_dict(counting_letters)
+    print(f"""
+============ BOOKBOT ============
+Analyzing book found at books/frankenstein.txt...
+----------- Word Count ----------
+Found {count} total words
+--------- Character Count -------""")
+    for dictionary in sorted_dictionary:
+        char_dic = dictionary["char"]
+        count_dic = dictionary["num"]
+        if char_dic.isalpha():
+            print(f"{char_dic}: {count_dic}")                  
+print("============= END ===============")          
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -12,7 +24,7 @@ def get_book_text(path_to_file):
         return file_content
 
 from stats import get_word_count
-
 from stats import char_counter
+from stats import sorted_dict
 
 main()
--- stats.py
+++ stats.py
@@ -11,3 +11,17 @@ def char_counter(text):
         else:
             character_dict[l_letter] += 1
     return character_dict
+
+def sorted_dict(counting_letters):
+    list_of_dict = []
+    completed_sorted_letter_list = []
+    #making a list
+    for character, count in counting_letters.items():
+        char_info = {"char": character, "num": count}
+        list_of_dict.append(char_info)
+    #making a sorting function for said list
+    def sort_fnc(sorted_dic_f):
+        return sorted_dic_f["num"]
+    list_of_dict.sort(reverse=True, key=sort_fnc)    
+    return list_of_dict
+    
