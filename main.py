f96863ec667d9808478bca51c98bb819de4152c8
L4K6 <je.pavel@seznam.cz>
2025-09-11 00:00:17 +0200

    created book reportfunctions

--- main.py
+++ main.py
@@ -1,12 +1,34 @@
 from stats import num_of_words
 from stats import count_characters
+from stats import sort_dict
+from stats import create_alphabetic_dict
 
 def main():
     book_path = "./books/frankenstein.txt"
+    
     text = get_book_text(book_path)
-    num_of_words(text)
+    num_words = num_of_words(text)
+
     characters = count_characters(text)
-    print(characters)
+
+    sorted_dictionary = sort_dict(characters)
+
+    sorted_dictionary_only_alpha = create_alphabetic_dict(sorted_dictionary)
+
+    
+
+    print (f""" 
+============ BOOKBOT ============
+Analyzing book found at {book_path}...
+----------- Word Count ----------
+Found {num_words} total words
+--------- Character Count -------""")
+
+    for item in sorted_dictionary_only_alpha:
+        print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
+
 
 def get_book_text(filepath):
     with open(filepath) as f:
--- stats.py
+++ stats.py
@@ -1,11 +1,7 @@
 def num_of_words(text):
     words = text.split()
-    num = 0
-    for word in words:
-        num += 1
-    print(f"{num} words found in the document")
-
-
+    return len(words)
+    
 def count_characters(text):
     letters_d = {}
     text = text.lower()
@@ -14,7 +10,25 @@ def count_characters(text):
             letters_d[letter] += 1
         else:
             letters_d[letter] = 1
-    
-    
-    
     return letters_d
+
+def help_sort_on(dictionary):
+    return dictionary["num"]
+
+def sort_dict(dictionary):
+    list_of_dictionaries = []
+    for x in dictionary:
+        new_dict = {}
+        new_dict["char"] = x
+        new_dict["num"] = dictionary[x]
+        list_of_dictionaries.append(new_dict)
+    list_of_dictionaries.sort(reverse=True, key=help_sort_on)
+    return list_of_dictionaries
+        
+def create_alphabetic_dict(sorted_dictionary):
+    sorted_alphabet_dict_list = []
+    for item in sorted_dictionary:
+        string = item["char"]
+        if string.isalpha():
+            sorted_alphabet_dict_list.append(item)
+    return sorted_alphabet_dict_list    
